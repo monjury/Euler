0a004e5ec37511cac19748ef30ca0db0b51ff210
samchandra <samchandra@localhost>
2014-01-21 19:00:08 -0800

    P - 2 Answer

--- Problems/src/P002.java
+++ Problems/src/P002.java
@@ -11,7 +11,7 @@ public class P002 {
 		 * By considering the terms in the Fibonacci sequence whose values do
 		 * not exceed four million, find the sum of the even-valued terms.
 		 * 
-		 * Answer : 
+		 * Answer : 4613732
 		 */
 		
 		int n = 1;
