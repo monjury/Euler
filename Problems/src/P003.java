a8ff2ee6fbd6cae4689a7b635e55e019df0bdfa1
sampriya <sampriya@localhost>
2014-02-06 21:13:12 -0800

    P-3

---
+++ Problems/src/P003.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class P003 {
+
+	
+	public static void calculate(long n) {
+		/*
+		 * The prime factors of 13195 are 5, 7, 13 and 29. What is the largest
+		 * prime factor of the number 600851475143 ?
+		 */		
+		
+		HashMap<Long, Boolean> map = new HashMap<Long, Boolean>();
+		long largest = 0;
+		long start = (long)Math.sqrt(n);
+		if(start % 2 == 0) {
+			start++;
+		}
+		for(long i = start; i > 2; i-=2) {
+			if(n % i == 0 && isPrime(i, map)) {
+				largest = i;
+				break;
+			}
+		}
+		System.out.println(largest);
+	}
+
+	private static boolean isPrime(long n, HashMap<Long, Boolean> map ) {
+		if(map.get(n) != null && map.get(n) == true) {
+			return true;
+		}
+		for(int i = 3; i < Math.sqrt(n); i+=2) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		map.put(n, true);
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		P003.calculate(600851475143L);
+	}
+}
